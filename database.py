ba8b14cac0b1f9547314690387849082feec0e4a
Miguel Torres <migueltorres@ucr-secure-43-10-13-222-39.wnet.ucr.edu>
2024-12-16 19:59:17 -0800

    added database.py file

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+def init_database():
+    connection = sqlite3.connect("dentalscheduler.db")
+    cursor = connection.cursor()
+
+    #Create tables
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS patients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        phone TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS procedures (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        duration INTEGER NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS appointments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        patient_id INTEGER,
+        procedure_id INTEGER,
+        appointment_time TEXT NOT NULL,
+        FOREIGN KEY (patient_id) REFERENCES patients(id),
+        FOREIGN KEY (procedure_id) REFERENCES procedures(id)
+    )
+    """)
+
+    connection.commit()
+    connection.close()
+
+if __name__ == "__main__":
+    init_database()
